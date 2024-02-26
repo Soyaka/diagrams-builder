6658d5ea49d5e8c7f80b06eb111a0edec14d2f33
soyaka <soyaka@soyaka.soyaka>
2024-02-26 17:55:27 -0500

    first establish

---
+++ app/_components/Header.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import Image from "next/image";
+export default function Header() {
+  return (
+    <header className="bg-black">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex gap-2 items-center">
+            <Image src="/logo-1.png" alt="logo" width={50} height={50} />
+            <h2 className=" text-white text-2xl font-bold">eraser</h2>
+          </div>
+          <div className="hidden md:block">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200/75"
+                    href="#"
+                  >
+                    {" "}
+                    About{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200/75"
+                    href="#"
+                  >
+                    {" "}
+                    Careers{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200/75"
+                    href="#"
+                  >
+                    {" "}
+                    History{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200/75"
+                    href="#"
+                  >
+                    {" "}
+                    Services{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200/75"
+                    href="#"
+                  >
+                    {" "}
+                    Projects{" "}
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-white transition hover:text-gray-200/75"
+                    href="#"
+                  >
+                    {" "}
+                    Blog{" "}
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className="sm:flex sm:gap-4">
+              <a
+                className="rounded-md bg-rose-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                href="#"
+              >
+                Login
+              </a>
+
+              <div className="hidden sm:flex">
+                <a
+                  className="rounded-md bg-blue-500 px-5 py-2.5 text-sm font-medium text-white"
+                  href="#"
+                >
+                  Register
+                </a>
+              </div>
+            </div>
+
+            <div className="block md:hidden">
+              <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
